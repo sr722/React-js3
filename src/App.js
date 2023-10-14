1c27ee57e5cd52dac50273117fa22ab09ecfd782
devarakonda sravani <sr7224>
2023-10-15 01:57:50 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles.css";
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class AgeCalculator extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dob: '',
+      age: null,
+    };
+  }
+
+  calculateAge = () => {
+    const dob = new Date(this.state.dob);
+    const currentDate = new Date();
+    const ageDiff = currentDate - dob;
+    const ageDate = new Date(ageDiff);
+    const calculatedAge = Math.abs(ageDate.getUTCFullYear() - 1970);
+
+    this.setState({
+      age: calculatedAge,
+    });
+  };
+
+  handleInputChange = (event) => {
+    this.setState({
+      dob: event.target.value,
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <h2>Age Calculator</h2>
+        <label htmlFor="dob">Enter your date of birth: </label>
+        <p></p>
+        <input
+          type="date"
+          id="dob"
+          value={this.state.dob}
+          onChange={this.handleInputChange}
+        />
+        <p></p>
+        <button  onClick={this.calculateAge} style={{color: "white"},{background: " skyblue"}}>Calculate Age</button>
+        
+
+        {this.state.age !== null && (
+          <p>Your age is: {this.state.age} years old</p>
+        )}
+      </div>
+    );
+  }
 }
 
-export default App;
+export default AgeCalculator;
+
